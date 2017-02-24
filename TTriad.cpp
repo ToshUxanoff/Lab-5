3e3e0b9fc3548d0e90851c5fc5d6fa82179ed60d
Уханов Антон <tosha.uhanoff@yandex.ru>
2017-02-24 14:49:13 +0300

    Create TTriad.cpp

---
+++ TTriad.cpp
@@ -0,0 +1,64 @@
+#include "stdafx.h"
+#include "TTriad.h"
+#include "iostream"
+
+TTriad::TTriad()
+{
+	First = 0;
+	Second = 0;
+	Third = 0;
+}
+TTriad::TTriad(int a, int b, int c)
+{
+	First = a;
+	Second = b;
+	Third = c;
+}
+void TTriad::Change(int a, int b, int c)
+{
+	First = a;
+	Second = b;
+	Third = c;
+}
+TTriad::~TTriad()
+{
+}
+bool TTriad::operator = (const TTriad& obj)
+{
+	if (First == obj.First && Second == obj.Second && Third == obj.Third)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+bool TTriad::operator > (const TTriad& obj)
+{
+	if ((First + Second + Third) > (obj.First + obj.Second + obj.Third))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+bool operator < (const TTriad& a, const TTriad& b)
+{
+	TTriad first(a);
+	TTriad second(b);
+	if (first > second)
+	{
+		return false;
+	}
+	else if (first = second)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
