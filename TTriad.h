9ac04a1603830f6a100f67a6d73eaa5d750dce0f
Уханов Антон <tosha.uhanoff@yandex.ru>
2017-02-24 14:46:58 +0300

    Create TTriad.h

---
+++ TTriad.h
@@ -0,0 +1,16 @@
+#pragma once
+class TTriad
+{
+public:
+	TTriad();
+	TTriad(int a, int b, int c);
+	~TTriad();
+	void Change(int a, int b, int c);
+	bool operator > (const TTriad& obj);
+	bool operator = (const TTriad& obj);
+protected:
+	int First;
+	int Second;
+	int Third;
+};
+bool operator < (const TTriad& a, const TTriad &b);
