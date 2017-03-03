e65fc3b79ab2675d3e54151cfdf3a7d51c8fcfe2
Уханов Антон <tosha.uhanoff@yandex.ru>
2017-03-03 20:22:28 +0300

    Update TDate.h

--- TDate.h
+++ TDate.h
@@ -5,10 +5,11 @@ class TDate :
 {
 public:
 	TDate(int a, int b, int c);
-	bool operator>(const TDate &obj);
-	bool operator==(const TDate &obj);
+	bool operator > (const TDate &obj) const;
+	bool operator==(const TDate &obj) const;
 	void Change(int day, int month, int year);
 	void GetDate();
+
 protected:
 };
-bool operator<(const TDate & a, const TDate & b);
+bool operator < (const TDate & a, const TDate & b);
