a2c87afeaedf3b93535ccf624257371e50932c1b
Уханов Антон <tosha.uhanoff@yandex.ru>
2017-03-03 20:24:14 +0300

    Update TTriad.h

--- TTriad.h
+++ TTriad.h
@@ -6,8 +6,9 @@ public:
 	TTriad(int a, int b, int c);
 	~TTriad();
 	void Change(int a, int b, int c);
-	bool operator > (const TTriad& obj);
-	bool operator == (const TTriad& obj);
+	bool operator > (const TTriad& obj) const;
+	bool operator == (const TTriad& obj) const;
+	
 protected:
 	int First;
 	int Second;
